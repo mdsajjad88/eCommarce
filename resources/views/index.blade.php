d4103bcba458398e4a51b31a2b491a28eccb3522
Md Sajjad Hossan <119941949+mdsajjad88@users.noreply.github.com>
2024-03-08 17:29:39 +0600

    route or view Change

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testing</title>
+</head>
+<body>
+    <h3>Hello my Bangladesh</h3>
+    <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem nesciunt delectus facilis totam perspiciatis! Dolorum numquam cum molestias ad enim?.
+    </p>
+</body>
+</html>
